b69d0d27cd85edd7d75619a92796c26ee4d2defb
Тимофей Филенко <timofey@Timofeys-MacBook-Pro.local>
2022-10-04 11:55:59 +0300

    classwork

---
+++ 20221004/1/prog.py
@@ -0,0 +1,12 @@
+l = eval(input())
+
+def f(arr, el):
+    if len(arr) == 1 and arr[0] != el:
+        return False
+    if arr[len(arr) // 2] == el:
+        return True
+    if arr[len(arr) // 2] < el:
+        return f(arr[len(arr) // 2:], el)
+    if arr[len(arr) // 2] > el:
+        return f(arr[:len(arr)//2], el)
+    
