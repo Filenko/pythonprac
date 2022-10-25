cb131ecf7170c640e862121fffcd6d8c7ae15a5e
Тимофей Филенко <timofey@Timofeys-MacBook-Pro.local>
2022-10-25 11:56:49 +0300

    123

---
+++ 20221025/1/prog.py
@@ -0,0 +1,14 @@
+from itertools import dropwhile, islice
+
+# def dsum():
+# 	i = 1
+# 	s = 0
+# 	while(1):
+# 		s += 1 / (i**2)
+# 		yield s
+# 		i += 1
+
+# print(*islice(dropwhile(lambda x: x <= 1.6, dsum()), 10))
+
+
+for 
