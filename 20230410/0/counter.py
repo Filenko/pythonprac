1ac591df4caf0427568c15361175d883f8b30c2d
timofey <timofey.filenko@yandex.ru>
2023-04-10 15:56:39 +0300

    Classwork 10 aplril

---
+++ 20230410/0/counter.py
@@ -0,0 +1,11 @@
+import sys
+import gettext
+import os
+
+popath = os.path.join(os.path.dirname(__file__), "po")
+
+translation = gettext.translation("counter", popath, fallback=True)
+_, ngettext = translation.gettext, translation.ngettext
+
+while s := sys.stdin.readline():
+    print(ngettext("{} word entered", "{} words entered", len(s.split())).format(len(s.split())))
