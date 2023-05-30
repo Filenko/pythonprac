d7014294432f0ae3a888601d9ada2bc5e02bf812
timofey <timofey.filenko@yandex.ru>
2023-05-30 03:31:21 +0300

    Add comments to server

--- 20230403/1/moodserver/__main__.py
+++ 20230403/1/moodserver/__main__.py
@@ -1,7 +1,7 @@
 """Main."""
 
 from .moodserver import main
-
+import asyncio
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
--- 20230403/1/moodserver/moodserver.py
+++ 20230403/1/moodserver/moodserver.py
@@ -3,6 +3,7 @@
 import asyncio
 import cowsay
 import io
+import random
 
 jgsbat = cowsay.read_dot_cow(io.StringIO(r"""
 $the_cow = <<EOC;
@@ -31,13 +32,34 @@ userMonsters = {"jgsbat": jgsbat}
 # monsters = [{pos: (0,0), hp: int, hello: string}]
 
 def checkMonsters(pos):
+    """
+    Check if a monster is present at the given position.
+
+    Args:
+        pos (tuple): A tuple representing the position to check.
+
+    Returns:
+        int: The index of the monster if present, -1 otherwise.
+    """
     global monsters
     for monster in enumerate(monsters):
         if monster[1]["pos"] == pos:
             return monster[0]
     return -1
+
+
 def move(xDir, yDir, pos):
-    """Move to right by xDir and to left by yDir."""
+    """
+    Move to a new position based on the given directions.
+
+    Args:
+        xDir (int): The direction to move in the x-axis.
+        yDir (int): The direction to move in the y-axis.
+        pos (tuple): The current position.
+
+    Returns:
+        list: A list containing a message about the move and the new position.
+    """
     x = pos[0] + xDir
     x += 10 if x < 0 else 0 + -10 if x > 9 else 0
     y = pos[1] + yDir
@@ -46,9 +68,20 @@ def move(xDir, yDir, pos):
     return [f"Moved to {pos}", pos]
 
 
-
 def addmon(x, y, name, hello, hp):
-    """Add monster with name, hp and message."""
+    """
+    Add a monster with a given name, greeting message, and health points at a specific position.
+
+    Args:
+        x (int): The x-coordinate of the position.
+        y (int): The y-coordinate of the position.
+        name (str): The name of the monster.
+        hello (str): The greeting message of the monster.
+        hp (int): The health points of the monster.
+
+    Returns:
+        tuple: A tuple containing a list of response messages and a boolean indicating whether to send to all clients.
+    """
     response = []
     sendAll = True
     if name not in cowsay.list_cows() and name not in userMonsters:
@@ -56,12 +89,12 @@ def addmon(x, y, name, hello, hp):
         sendAll = False
 
     global monsters
-    monsterHere = checkMonsters((x,y))
+    monsterHere = checkMonsters((x, y))
 
     if monsterHere != -1:
-        monsters[monsterHere] = {"greeting": hello, "name": name, "hp": hp, "pos": (x,y)}
+        monsters[monsterHere] = {"greeting": hello, "name": name, "hp": hp, "pos": (x, y)}
     else:
-        monsters.append({"pos":(x,y),"greeting": hello, "name": name, "hp": hp})
+        monsters.append({"pos": (x, y), "greeting": hello, "name": name, "hp": hp})
 
     response.append(f"Added monster {name} to {x}, {y} saying {hello}")
     if monsterHere != -1:
@@ -72,7 +105,15 @@ def addmon(x, y, name, hello, hp):
 
 
 def encounter(pos):
-    """Check if here is monster and return its message."""
+    """
+    Check if a monster is present at the given position and return its greeting message.
+
+    Args:
+        pos (tuple): The position to check.
+
+    Returns:
+        str: The greeting message of the monster if present, None otherwise.
+    """
     global monsters
 
     if (monsterId := checkMonsters(pos)) != -1:
@@ -88,7 +129,17 @@ def encounter(pos):
 
 
 def attack(name, weapon, pos):
-    """Attack monster with different weapons (damage)."""
+    """
+    Attack a monster with a specific weapon at a given position.
+
+    Args:
+        name (str): The name of the monster.
+        weapon (str): The weapon to use for the attack.
+        pos (tuple): The position of the attack.
+
+    Returns:
+        tuple: A tuple containing a list of response messages and a boolean indicating whether to send to all clients.
+    """
     global monsters
     monsterHere = checkMonsters(pos)
     response = []
@@ -113,7 +164,16 @@ def attack(name, weapon, pos):
 
 
 async def sendMessage(msg, me=None):
-    """Send message to all clients or to single one."""
+    """
+    Send a message to all clients or to a single client.
+
+    Args:
+        msg (str): The message to send.
+        me (str): The identifier of the client to send the message to. If None, the message is sent to all clients.
+
+    Returns:
+        None
+    """
     if not me:
         for client in clients:
             await clients[client].put(msg)
@@ -121,8 +181,58 @@ async def sendMessage(msg, me=None):
         await clients[me].put(msg)
 
 
+async def moveMonster():
+    """
+    Move a random monster in a random direction and encounter players.
+
+    This function runs indefinitely, moving a random monster every 30 seconds.
+
+    Returns:
+        None
+    """
+    global clients, positions, monsters
+    dirs = {
+        (1, 0): "right",
+        (0, 1): "down",
+        (-1, 0): "left",
+        (0, -1): "up"
+    }
+    while True:
+        await asyncio.sleep(30)
+        if len(monsters) == 0:
+            continue
+
+        success = False
+        while not success:
+            monster = random.randint(0, len(monsters) - 1)
+            monsterPos = monsters[monster]["pos"]
+            direction = random.choice(list(dirs.keys()))
+            directionName = dirs[direction]
+            newMonsterPos = move(direction[0], direction[1], monsterPos)[1]
+            if checkMonsters(newMonsterPos) == -1:
+                monsters[monster]["pos"] = newMonsterPos
+                success = True
+
+        for client in clients:
+            msg = [f'{monsters[monster]["name"]} moved one cell {directionName}']
+            if enc := encounter(positions[client]):
+                msg.append(enc)
+            await clients[client].put("\n".join(msg))
+
+
 async def MUDhandler(reader, writer):
-    """Start the game and provide all logic."""
+    """
+    Handle a client connection for the MUD game.
+
+    This function reads commands from the client, processes them, and sends responses back to the client.
+
+    Args:
+        reader (StreamReader): The stream reader object for reading data from the client.
+        writer (StreamWriter): The stream writer object for sending data to the client.
+
+    Returns:
+        None
+    """
     me = "{}:{}".format(*writer.get_extra_info('peername'))
     print(me)
     clients[me] = asyncio.Queue()
@@ -140,7 +250,7 @@ async def MUDhandler(reader, writer):
                             await sendMessage("This nickname is already taken!", me)
                         else:
                             nicknames[me] = nickname
-                            positions[me] = (0,0)
+                            positions[me] = (0, 0)
                             await sendMessage(f"{nickname} join to MUD")
                     case ["move", x, y]:
                         req, newPos = move(int(x), int(y), positions[me])
@@ -180,10 +290,14 @@ async def MUDhandler(reader, writer):
 
 
 async def main():
-    """Start server with main function."""
+    """
+    Start the server and the MUD game.
+
+    This function starts the server, waits for client connections, and starts the game logic.
+
+    Returns:
+        None
+    """
     server = await asyncio.start_server(MUDhandler, '0.0.0.0', 1337)
     async with server:
-        await server.serve_forever()
-
-
-asyncio.run(main())
+        await asyncio.gather(server.serve_forever(), moveMonster())
