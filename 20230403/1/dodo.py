6e3d974b35a211d41d58c421915d8577e0b51a9f
timofey <timofey.filenko@yandex.ru>
2023-05-30 11:40:05 +0300

    Final

---
+++ 20230403/1/dodo.py
@@ -0,0 +1,26 @@
+DOIT_CONFIG = {"default_tasks": ["html"]}
+def task_html():
+    return {
+        'actions': [
+            'make html',
+            ],
+        }
+
+
+def task_wheel_server():
+    return {
+        'actions': ['python3 -m build -n -w moodserver'],
+        }
+
+
+def task_wheel_client():
+    return {
+        'actions': ['python3 -m build -n -w moodclient'],
+        }
+
+
+def task_wheels():
+    return {
+        'actions': [],
+        'task_dep': ['wheel_server', 'wheel_client'],
+        }
